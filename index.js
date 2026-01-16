35dad053e75eb8d97b12e9c36476f8f64d7ae8b2
M Saad Mustafa <msaadmustafa091@gmail.com>
2026-01-16 10:42:08 -0800

    adding vercel json

--- index.js
+++ index.js
@@ -1,23 +1,4 @@
-// import connectDB from "./db/index.js";
-// import { app } from "./app.js";
-// import dotenv from "dotenv";
 
-// const PORT = 8000; // Directly define port
-
-// dotenv.config({
-//   path:'./env'
-// })
-// connectDB()
-//   .then(() => {
-//     app.listen(PORT, () => {
-//       console.log(`🚀 Server running on port ${PORT}`);
-//     });
-//   })
-//   .catch((err) => {
-//     console.log("❌ Mongo DB Connection failed:", err);
-//   });
-
-// Load .env first
 
 import connectDB from "./db/index.js";
 import { app } from "./app.js";
--- routes/route.js
+++ routes/route.js
@@ -1,30 +1,29 @@
 import { Router } from "express";
 import { upload } from "../middlewares/multer.middleware.js";
-import { GetUser, LoginUser, LogoutUser, Registeruser, UpdateProfile } from "../controllers/user.controller.js";
+import {
+  GetUser,
+  LoginUser,
+  LogoutUser,
+  Registeruser,
+  UpdateProfile,
+} from "../controllers/user.controller.js";
 import { Verijwt } from "../middlewares/Auth.middleware.js";
 
-
-
-
-const  router = Router();
-
-
-
+const router = Router();
 
 router.route("/register").post(
   upload.fields([
     {
       name: "avatar",
-      maxCount: 1
-    }
+      maxCount: 1,
+    },
   ]),
-  Registeruser
+  Registeruser,
 );
-router.route('/login').post(LoginUser)
-router.route('/updateprofile/:id').put(
-  upload.single("avatar"),Verijwt,UpdateProfile
-)
-router.route('/logout',).post(Verijwt,LogoutUser)
-router.route('/getuser').get(Verijwt,GetUser)
-export  default router
-    
+router.route("/login").post(LoginUser);
+router
+  .route("/updateprofile/:id")
+  .put(upload.single("avatar"), Verijwt, UpdateProfile);
+router.route("/logout").post(Verijwt, LogoutUser);
+router.route("/getuser").get(Verijwt, GetUser);
+export default router;
